8c092ffc8c54408a2fa0a416f53bbeff106ddabc
Foofdof <foofdof@gmail.com>
2024-11-15 22:11:10 +0300

    Integral equations. graphs updated

--- Integral Equations/main.py
+++ Integral Equations/main.py
@@ -36,16 +36,19 @@ def task1():
 
     grid_p = np.linspace(t_min, t_max, 100)
 
-    tck_1 = interpolate.splrep(grid, sol, k=3)
-    tck_2 = interpolate.interp1d(grid2, sol2.flatten(), kind='linear')
+    tck_1 = interpolate.splrep(grid_g.flatten(), sol_g.flatten(), k=3)
+    tck_1_tr = interpolate.splrep(grid, sol, k=3)
+    tck_2 = interpolate.splrep(grid2_g.flatten(), sol2_g.flatten(), k=2)
+    # tck_2_tr = interpolate.splrep(grid2_g.flatten(), sol2_g.flatten(), k=2)
 
     fig, ax = plt.subplots(1)
     ax.scatter(grid, sol, label='$x(t), n=10$', color='blue', marker='o')
     ax.scatter(grid2, sol2, label='$x(t), n=3$', color='blue', marker='^')
-    ax.plot(grid, interpolate.splev(grid, tck_1, der=0), label='$x(t)_{int}$', color='blue')
 
     ax.scatter(grid_g, sol_g, label='$x(t), n=10$', color='black', marker='o')
-    ax.scatter(grid2_g, sol2_g, label='$x(t), n=3$', color='black', marker='^')
+    ax.scatter(grid2_g, sol2_g, label='$x(t), n=3$', color='gray', marker='^')
+    ax.plot(grid_p, interpolate.splev(grid_p, tck_1, der=0), label='$x(t)_{int}$ $n=10$', color='black')
+    ax.plot(grid_p, interpolate.splev(grid_p, tck_2, der=0), label='$x(t)_{int}$ $n=3$', color='gray')
 
     ax.plot(grid_p, p_sol_1(grid_p, lmbd), label='$x_p(t), $', color='red')
 
@@ -56,8 +59,23 @@ def task1():
     plt.title('Fredholm int. eq. solution', fontsize=16, fontweight='bold')
     plt.show()
 
+    fig, ax = plt.subplots(1)
+    ax.plot(grid_p, np.abs(p_sol_1(grid_p, lmbd) - interpolate.splev(grid_p, tck_1, der=0)),
+            label='$x(t)_{int}$ $n=10$ leg', color='black')
+    ax.plot(grid_p, np.abs(p_sol_1(grid_p, lmbd) - interpolate.splev(grid_p, tck_1_tr, der=0)),
+            label='$x(t)_{int}$ $n=10$ tr', color='blue')
+    ax.plot(grid_p, np.abs(p_sol_1(grid_p, lmbd) - interpolate.splev(grid_p, tck_2, der=0)),
+            label='$x(t)_{int}$ $n=10$ tr', color='gray')
+
+    ax.set_xlabel('t')
+    ax.set_ylabel('x(t)')
+    ax.legend()
+    plt.grid(True, which='both', linestyle='--', linewidth=0.5, color='gray')
+    plt.title('Abs Error', fontsize=16, fontweight='bold')
+    plt.show()
+
     print("n=10: ", interpolate.splev(sig_points, tck_1, der=0))
-    print("n=3: ", tck_2(sig_points))
+    print("n=3: ", interpolate.splev(sig_points, tck_2, der=0))
 
 
 def task2():
