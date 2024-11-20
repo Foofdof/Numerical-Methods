2f51a0b5650d9140ccfd3df097c3dfe4333a7ebe
Foofdof <foofdof@gmail.com>
2024-11-20 20:03:48 +0300

    girmethods

---
+++ GirMethod/main.py
@@ -0,0 +1,180 @@
+import numpy as np
+from methods import (
+    euler_method, euler_method2,
+    modified_euler_method1,
+    implicit_euler_method,
+    gear_method, runge2
+)
+import matplotlib.pyplot as plt
+
+colors = {
+    0: 'red',
+    1: 'blue',
+    2: 'green',
+    3: 'yellow',
+}
+A_matrix = np.array([
+    [0, -1, 100],
+    [0, 0, -100]
+])
+# var_list = [t, x1, x2, x3 ...]
+# funcs = [dx1/dt = F1(vl), dx2/dt = F2(vl) ...]
+funcs = np.array([
+    lambda var_list: -var_list[1] + 100 * var_list[2],
+    lambda var_list: -100 * var_list[2],
+])
+initial_conditions = np.array([1, 1], dtype=np.float64)
+sigma_classic = np.array([1 / 6, 2 / 6, 2 / 6, 1 / 6])
+
+
+def task_1(a, b, tau):
+    t_grid = np.arange(a, b + tau / 2, tau)
+
+    sol1 = euler_method(funcs, initial_conditions, t_grid)
+    sol2 = euler_method2(A_matrix, initial_conditions, t_grid)
+    pr_sol = np.array([
+        1 / 99. * np.exp(-100 * t_grid) * (-100 + 199 * np.exp(99 * t_grid)),
+        np.exp(-100 * t_grid)
+    ])
+    fig, ax = plt.subplots(4)
+    for j, sol in enumerate([sol1, sol2, np.abs(sol1 - pr_sol), np.abs(sol2 - pr_sol)]):
+        for i in range(sol.shape[0]):
+            ax[j].plot(t_grid, sol[i], c=colors[i], label=f'$x_{i + 1}(t)$')
+
+    for a in ax:
+        a.legend()
+        a.grid(True, which='both', linestyle='--', linewidth=0.5, color='gray')
+    fig.suptitle(f'Euler, tau {tau}', fontsize=16, fontweight='bold')
+    plt.tight_layout(rect=[0, 0, 1, 0.95])
+    plt.grid(True, which='both', linestyle='--', linewidth=0.5, color='gray')
+    plt.show()
+
+
+def task_2(a, b, tau):
+    t_grid = np.arange(a, b + tau / 2, tau)
+
+    sol1 = modified_euler_method1(funcs, initial_conditions, t_grid)
+    pr_sol = np.array([
+        1 / 99. * np.exp(-100 * t_grid) * (-100 + 199 * np.exp(99 * t_grid)),
+        np.exp(-100 * t_grid)
+    ])
+    fig, ax = plt.subplots(2)
+    for j, sol in enumerate([sol1, np.abs(sol1 - pr_sol)]):
+        for i in range(sol.shape[0]):
+            ax[j].plot(t_grid, sol[i], c=colors[i], label=f'$x_{i + 1}(t)$')
+
+    for a in ax:
+        a.legend()
+        a.grid(True, which='both', linestyle='--', linewidth=0.5, color='gray')
+    fig.suptitle(f'Modified Euler, tau {tau}', fontsize=16, fontweight='bold')
+    plt.tight_layout(rect=[0, 0, 1, 0.95])
+    plt.grid(True, which='both', linestyle='--', linewidth=0.5, color='gray')
+    plt.show()
+
+
+def task_3(a, b, tau):
+    t_grid = np.arange(a, b + tau / 2, tau)
+
+    sol1 = implicit_euler_method(funcs, initial_conditions, t_grid)
+    pr_sol = np.array([
+        1 / 99. * np.exp(-100 * t_grid) * (-100 + 199 * np.exp(99 * t_grid)),
+        np.exp(-100 * t_grid)
+    ])
+    fig, ax = plt.subplots(2)
+    for j, sol in enumerate([sol1, np.abs(sol1 - pr_sol)]):
+        for i in range(sol.shape[0]):
+            ax[j].plot(t_grid, sol[i], c=colors[i], label=f'$x_{i + 1}(t)$')
+
+    for a in ax:
+        a.legend()
+        a.grid(True, which='both', linestyle='--', linewidth=0.5, color='gray')
+    fig.suptitle(f'Implicit Euler, tau {tau}', fontsize=16, fontweight='bold')
+    plt.tight_layout(rect=[0, 0, 1, 0.95])
+    plt.grid(True, which='both', linestyle='--', linewidth=0.5, color='gray')
+    plt.show()
+
+
+def task_4(a, b, tau, m, base_method_c='ie'):
+    t_grid = np.arange(a, b + tau / 2, tau)
+
+    sol1 = gear_method(
+        funcs=funcs,
+        init_cond=initial_conditions,
+        t_grid=t_grid,
+        base_method_c=base_method_c,
+        m=m,
+    )
+
+    pr_sol = np.array([
+        1 / 99. * np.exp(-100 * t_grid) * (-100 + 199 * np.exp(99 * t_grid)),
+        np.exp(-100 * t_grid)
+    ])
+    fig, ax = plt.subplots(2)
+    for j, sol in enumerate([sol1, np.abs(sol1 - pr_sol)]):
+        for i in range(sol.shape[0]):
+            ax[j].plot(t_grid, sol[i], c=colors[i], label=f'$x_{i + 1}(t)$')
+
+    for a in ax:
+        a.legend()
+        a.grid(True, which='both', linestyle='--', linewidth=0.5, color='gray')
+    fig.suptitle(f'Implicit Gear, tau: {tau}, m: {m}', fontsize=16, fontweight='bold')
+    plt.tight_layout(rect=[0, 0, 1, 0.95])
+    plt.show()
+
+    return sol1
+
+
+def part2(a, b, tau, m, base_method_c='ie'):
+    funcs = np.array([
+        lambda var_list: 2*np.sin(var_list[0]) + var_list[1],
+    ])
+    initial_conditions = np.array([1], dtype=np.float64)
+
+    t_grid = np.arange(a, b + tau / 2, tau)
+
+    sol1 = runge2(
+        funcs=funcs,
+        y_ic=initial_conditions,
+        x_range=t_grid,
+        mode=0,
+        sigma_list=sigma_classic
+    )
+
+
+
+    pr_sol = np.array([
+        np.exp(t_grid)*(1+1)-np.sin(t_grid)-np.cos(t_grid),
+    ])
+
+    fig, ax = plt.subplots(2)
+    for j, sol in enumerate([sol1, np.abs(sol1 - pr_sol)]):
+        for i in range(sol.shape[0]):
+            ax[j].plot(t_grid, sol[i], c=colors[i], label=f'$x_{i + 1}(t)$')
+
+    for a in ax:
+        a.legend()
+        a.grid(True, which='both', linestyle='--', linewidth=0.5, color='gray')
+    fig.suptitle('$Runge_{class} $'+' '+'$ tau: {tau}, m: {m}$', fontsize=16, fontweight='bold')
+    plt.tight_layout(rect=[0, 0, 1, 0.95])
+    plt.show()
+
+    return sol1
+
+
+if __name__ == '__main__':
+    # part 1
+
+    # for tau in (0.1, 0.01, 0.001):
+    #     task_1(0, 1, tau)
+    #     task_2(0, 1, tau)
+    #     task_3(0, 1, tau)
+
+    # for m in (1, 2, 4):
+    #     task_4(0, 1, 0.01, m, base_method_c='ie')
+    #
+    # gear_1 = task_4(0, 1, 0.01, 1)
+    # gear_2 = task_4(0, 1, 0.01, 2, base_method_c=gear_1[:, :2])
+    # gear_4 = task_4(0, 1, 0.01, 4, base_method_c=gear_2[:, :4])
+
+    # part 2
+    part2(0, 1, 0.01, 4)
---
+++ GirMethod/methods.py
@@ -0,0 +1,224 @@
+import numpy as np
+
+
+def euler_method(funcs, init_cond, t_grid):
+    sol = np.zeros((funcs.shape[0], t_grid.shape[0]))
+    sol[:, 0] = init_cond
+
+    for i in range(1, len(t_grid)):
+        delta_t = t_grid[i] - t_grid[i - 1]
+        var_list = np.concatenate(([t_grid[i - 1]], sol[:, i - 1]))
+        for j, func in enumerate(funcs):
+            sol[j, i] = sol[j, i - 1] + delta_t * func(var_list)
+
+    return sol
+
+
+def euler_method2(A_matrix, init_cond, t_grid):
+    sol = np.zeros((A_matrix.shape[0], t_grid.shape[0]))
+    sol[:, 0] = init_cond
+
+    for i in range(1, len(t_grid)):
+        delta_t = t_grid[i] - t_grid[i - 1]
+        var_list = np.concatenate(([t_grid[i - 1]], sol[:, i - 1]))
+        sol[:, i] = sol[:, i - 1] + delta_t * np.dot(A_matrix, var_list)
+
+    return sol
+
+
+def modified_euler_method1(funcs, init_cond, t_grid):
+    sol = np.zeros((funcs.shape[0], t_grid.shape[0]))
+    sol[:, 0] = init_cond
+
+    for i in range(1, len(t_grid)):
+        delta_t = t_grid[i] - t_grid[i - 1]
+        # prediction
+        pred = np.zeros(funcs.shape[0])
+        var_list = np.concatenate(([t_grid[i - 1]], sol[:, i - 1]))
+        for j, func in enumerate(funcs):
+            pred[j] = sol[j, i - 1] + delta_t * func(var_list)
+        # correction
+        var_list_2 = np.concatenate(([t_grid[i]], pred))
+        for j, func in enumerate(funcs):
+            sol[j, i] = sol[j, i - 1] + delta_t * (func(var_list_2) + func(var_list)) / 2
+
+    return sol
+
+
+def get_k_list(x, y, func, h, mode):
+    k_list = np.zeros(4)
+    if mode != 0:
+        k_list[0] = func((x, y))
+        k_list[1] = func((x + 1 / 3 * h, y + h * k_list[0] * 1 / 3))
+        k_list[2] = func((x + 2 / 3 * h, y + h * (k_list[1] * 1 - 1 / 3 * k_list[0])))
+        k_list[3] = func((x + h, y + h * (k_list[2] - k_list[1] + k_list[0])))
+    else:
+        k_list[0] = func((x, y))
+        k_list[1] = func((x + 1 / 2 * h, y + h * k_list[0] * 1 / 2))
+        k_list[2] = func((x + 1 / 2 * h, y + h * k_list[1] * 1 / 2))
+        k_list[3] = func((x + h, y + h * k_list[2]))
+    return k_list
+
+
+def runge2(funcs, y_ic, x_range, mode, sigma_list):
+    y_ic = np.array(y_ic, dtype=float)
+    num_eq = len(funcs)
+    num_steps = len(x_range)
+
+    y_list = np.zeros((num_eq, num_steps))
+    y_list[:, 0] = y_ic
+
+    for idx in range(1, num_steps):
+        h = x_range[idx] - x_range[idx-1]
+        x_prev = x_range[idx - 1]
+        y_prev = y_ic.copy()
+        y_new = y_prev.copy()
+
+        for i, func in enumerate(funcs):
+            k = get_k_list(x_prev, y_prev, func, h, mode)
+            summ = np.dot(sigma_list, k)
+            y_new[i] += h * summ
+
+        y_ic = y_new
+        y_list[:, idx] = y_new
+
+    return y_list
+
+
+def implicit_euler_method(funcs, init_cond, t_grid, max_iter=1e3, delta=1e-6, phi=None):
+    x = init_cond.copy()
+    x_prev = x
+    sol = np.zeros((funcs.shape[0], t_grid.shape[0]))
+    sol[:, 0] = init_cond
+
+    for i in range(1, len(t_grid)):
+        delta_t = t_grid[i] - t_grid[i - 1]
+        t = t_grid[i]
+
+        def phi(x):
+            var_list = np.hstack(([t], x))
+            f = np.array([func(var_list) for func in funcs])
+            return x - x_prev - delta_t * f
+
+        # def phi(x):
+        #     var_list = np.concatenate(([t], x))
+        #     return x - x_prev - delta_t * np.array([func(var_list) for func in funcs])
+        #
+        # def jacobian(x):
+        #     var_list = np.hstack(([t], x))
+        #     jacobian_matrix = np.zeros((len(x), len(x)))
+        #     for j in range(len(x)):
+        #         x_plus, x_minus = x.copy(), x.copy()
+        #         x_plus[j] += delta
+        #         x_minus[j] -= delta
+        #         var_list_plus = np.hstack(([t + delta_t], x_plus))
+        #         var_list_minus = np.hstack(([t - delta_t], x_minus))
+        #         jacobian_matrix[:, j] = (
+        #             np.array([func(var_list_plus) for func in funcs]) -
+        #             np.array([func(var_list_minus) for func in funcs])
+        #         ) / (2 * delta)
+        #
+        #     return np.eye(len(x)) + jacobian_matrix * -delta_t
+
+        def jacobian(x):
+            var_list = np.hstack(([t], x))
+            f0 = np.array([func(var_list) for func in funcs])
+            n = len(x)
+            jacobian_matrix = np.zeros((n, n))
+            for j in range(n):
+                x_perturbed = x.copy()
+                x_perturbed[j] += delta
+                var_list_perturbed = np.hstack(([t], x_perturbed))
+                f_perturbed = np.array([func(var_list_perturbed) for func in funcs])
+                jacobian_matrix[:, j] = (f_perturbed - f0) / delta
+            return np.eye(n) - delta_t * jacobian_matrix
+
+        for iteration in range(int(max_iter)):
+            phi_m = jacobian(x)
+            phi_x = phi(x)
+            delta_x = np.dot(np.linalg.inv(phi_m), phi_x)
+            x = x - delta_x
+            err = np.linalg.norm(delta_x, ord=2)
+            if err < 1e-6:
+                break
+
+        sol[:, i] = x
+        x_prev = x
+
+    return sol
+
+
+def gear_method(funcs, init_cond, t_grid, m, base_method_c, max_iter=1e3, delta=1e-6):
+    sol = np.zeros((funcs.shape[0], t_grid.shape[0]))
+    b_list = np.array([1])
+    a_matrix = np.ones((m + 1, m + 1))
+    a_matrix[1:, 0] = 0
+    for i, row in enumerate(a_matrix):
+        a_matrix[i, 1:] *= np.array([j ** i for j in range(1, m + 1)])
+
+    rb = np.zeros((m + 1, 1))
+    rb[1, 0] = -1
+    a_list = np.linalg.solve(a_matrix, rb).flatten()
+
+    if type(base_method_c) is str:
+        if base_method_c == 'ie':
+            first_m_sol = implicit_euler_method(funcs, init_cond, t_grid[:m], max_iter=max_iter, delta=delta)
+        elif base_method_c == 'me':
+            first_m_sol = modified_euler_method1(funcs, init_cond, t_grid[:m])
+        elif base_method_c == 'e':
+            first_m_sol = euler_method(funcs, init_cond, t_grid[:m])
+        elif base_method_c == 'rk':
+            sigma_classic = np.array([1 / 6, 2 / 6, 2 / 6, 1 / 6])
+            first_m_sol = runge2(funcs, init_cond, t_grid[:m], 0, sigma_classic)
+    else:
+        if base_method_c.shape[1] != m:
+            raise Exception('Initial sols does not match the m')
+        else:
+            first_m_sol = base_method_c
+
+    sol[:, :m] += first_m_sol
+
+    x = sol[:, m].flatten()
+
+    for i in range(m, t_grid.shape[0]):
+        delta_t = t_grid[i] - t_grid[i - 1]
+        t = t_grid[i]
+
+        def phi(x):
+            var_list = np.hstack(([t], x))
+            f = np.array([func(var_list) for func in funcs])
+            res = x.copy()
+            for j in range(1, m+1):
+                res += sol[:, i-j]*a_list[j]/a_list[0]
+
+            return res - delta_t*f/a_list[0]
+
+        def jacobian(x):
+            var_list = np.hstack(([t], x))
+            f0 = np.array([func(var_list) for func in funcs])
+            n = len(x)
+            jacobian_matrix = np.zeros((n, n))
+            for j in range(n):
+                x_perturbed = x.copy()
+                x_perturbed[j] += delta
+                var_list_perturbed = np.hstack(([t], x_perturbed))
+                f_perturbed = np.array([func(var_list_perturbed) for func in funcs])
+                jacobian_matrix[:, j] = (f_perturbed - f0) / delta
+            return np.eye(n) - delta_t * jacobian_matrix
+
+        for iteration in range(int(max_iter)):
+            phi_m = jacobian(x)
+            phi_x = phi(x)
+            delta_x = np.dot(np.linalg.inv(phi_m), phi_x)
+            x = x - delta_x
+            err = np.linalg.norm(delta_x, ord=2)
+            if err < 1e-6:
+                break
+
+        sol[:, i] = x
+
+    return sol
+
+
+def adams_methods(funcs, init_cond, t_grid, m, base_method_c, max_iter=1e3, delta=1e-6):
+    
